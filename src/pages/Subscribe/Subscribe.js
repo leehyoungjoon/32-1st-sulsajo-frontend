513cde595e2f3dc1deb04860bf5ff3369715db5c
leehyoungjoon <92970761+leehyoungjoon@users.noreply.github.com>
2022-04-28 21:16:28 +0900

    기능구현 : carousel

--- src/pages/Subscribe/Subscribe.js
+++ src/pages/Subscribe/Subscribe.js
@@ -2,17 +2,13 @@ import React, { useState } from 'react';
 import './Subscribe.scss';
 const Subscribe = () => {
   const [index, setIndex] = useState(0);
-  // const clickhandler = ({ target }) => {
-  //   target.className === 'leftIcon' ? setIndex(index - 1) : setIndex(index + 1);
-  // };
+
   const leftclickhandler = ({ target }) => {
-    return setIndex(index - 1);
+    return index !== 0 && setIndex(index - 1);
   };
   const rightclickhandler = ({ target }) => {
-    return setIndex(index + 1);
+    return index !== 2 && setIndex(index + 1);
   };
-
-  // console.log(index);
 
   return (
     <div className="mainWrap">
@@ -33,7 +29,6 @@ const Subscribe = () => {
               <div className="header">
                 <span className="box">담와박스</span>
                 <span className="price">39,000원 / 월</span>
-                {/* <div>O</div> */}
               </div>
               <div className="boxComment">
                 다양한 주종을 경험할 수 있는 담와박스
@@ -55,13 +50,11 @@ const Subscribe = () => {
             src="./images/damhwabox_soju.png"
             alt="우측이미지"
           />
-          {/* <div className="rightComment"> */}
           <div className="rightComment">
             <div className="rightBox">
               <div className="header">
                 <span className="box">증류주 담와박스</span>
                 <span className="price">49,000원 / 월</span>
-                {/* <div>O</div> */}
               </div>
               <div className="boxComment">
                 다양한 주종을 경험할 수 있는 담와박스
@@ -76,11 +69,14 @@ const Subscribe = () => {
               <button className="hintOpen">힌트공개</button>
             </div>
           </div>
-          {/* </div> */}
         </div>
       </div>
       <div className="mainBanner">
-        <img className="mainBannerImg" src="./images/landing-banner-pc1.png" />
+        <img
+          className="mainBannerImg"
+          src="./images/landing-banner-pc1.png"
+          alt="상자이미지"
+        />
       </div>
       <div className="firstMainText">한 달을 특별하게 해주는</div>
       <div className="secondMainText">가장 편리한 방법</div>
@@ -88,11 +84,11 @@ const Subscribe = () => {
       <div className="firstSubscribeText">전통주 구독을</div>
       <div className="secondSubscribeText">술담와로 시작해야 하는 이유</div>
       <div className="landingBanner">
-        <img src="./images/landing-banner-pc2.png" />
+        <img src="./images/landing-banner-pc2.png" alt="술잔이미지" />
       </div>
       <div className="descriptionWrapper">
         <div className="landingIcon">
-          <img src="./images/landing-icon1.png" />
+          <img src="./images/landing-icon1.png" alt="트럭이미지" />
           <div className="firstDescription">
             유일하게 문 앞 배송이 가능한 술
           </div>
@@ -100,7 +96,7 @@ const Subscribe = () => {
           <div className="thirdDescription">일상 속 소중한 담와</div>
         </div>
         <div className="landingIcon">
-          <img src="./images/landing-icon2.png" />
+          <img src="./images/landing-icon2.png" alt="병체크이미지" />
           <div className="firstDescription">술에 진심인 소믈리에 픽</div>
           <div className="secondDescription">
             전국 1,200개 이상의 양조장 술 중
@@ -108,7 +104,7 @@ const Subscribe = () => {
           <div className="thirdDescription">잠재적 ‘인생술’만 골라 받기</div>
         </div>
         <div className="landingIcon">
-          <img src="./images/landing-icon3.png" />
+          <img src="./images/landing-icon3.png" alt="술병가격이미지" />
           <div className="firstDescription">가성비도 잊지 않았어요!</div>
           <div className="secondDescription">단품 구매보다</div>
           <div className="thirdDescription">평균 12% 더 저렴한 술로 구성</div>
@@ -128,19 +124,19 @@ const Subscribe = () => {
             }}
           >
             <div className="dontWorryBox">
-              <img src="./images/landing-slide-icon2.png" />
+              <img src="./images/landing-slide-icon1.png" alt="종이미지" />
               <div>결제되기 3일 전</div>
               <div>이번 달 담와박스 구성의 힌트와</div>
               <div>결제 알림 문자를 보내드려요.</div>
             </div>
             <div className="dontWorryBox">
-              <img src="./images/landing-slide-icon2.png" />
+              <img src="./images/landing-slide-icon2.png" alt="병이미지" />
               <div>노련한 소믈리에들이</div>
               <div>매달 블라인드 테이스팅을 통해</div>
               <div>누군가의 '인생술'이 될 수 있는 술들을 선별합니다.</div>
             </div>
             <div className="dontWorryBox">
-              <img src="./images/landing-slide-icon2.png" />
+              <img src="./images/landing-slide-icon3.png" alt="정지이미지" />
               <div>이번 달 구독이 힘들 것 같다면</div>
               <div>언제든 '쉬어가기' 할 수 있어요.</div>
               <div>당연히, 금액은 청구되지 않아요.</div>
@@ -164,7 +160,7 @@ const Subscribe = () => {
         <div>HyeongJoon</div>
       </div>
       <div className="converSationWrapper">
-        <img src="./images/bubble_conversation.png" />
+        <img src="./images/bubble_conversation.png" alt="말풍선이미지" />
       </div>
       <div className="tripDescription">
         자, 이제 나만의 '인생술 찾기 여정'을
