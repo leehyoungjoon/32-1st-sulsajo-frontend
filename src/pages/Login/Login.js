3ffd873d25a78b666bcf092b83e21fc15f3f63ab
leehyoungjoon <92970761+leehyoungjoon@users.noreply.github.com>
2022-04-28 17:28:52 +0900

    리뷰반영

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -45,60 +45,56 @@ const Login = () => {
     idInput.includes('@') && pwInput.match(/^(?=.*[a-zA-Z])((?=.*\d)).{8,16}$/);
 
   return (
-    <div>
-      <div className="loginWrap">
-        <p className="topLogin">로그인</p>
-        <form className="inputWrap" onSubmit={goToLogin}>
-          <div className="topEmail">이메일</div>
-          <div className="loginWrapper">
-            <input
-              className="inputId"
-              type="text"
-              placeholder="이메일을 입력해주세요"
-              onChange={handleIdInput}
-            />
-            <span className="separator" />
-          </div>
-          <div className="topEmail">비밀번호</div>
-          <div className="loginWrapper">
-            <input
-              className="inputPw"
-              type="password"
-              placeholder="비밀번호을 입력해주세요"
-              onChange={handlePwInput}
-            />
-            <span className="separator" />
-          </div>
+    <div className="Login">
+      <p className="topLogin">로그인</p>
+      <form className="inputWrap" onSubmit={goToLogin}>
+        <div className="topEmail">이메일</div>
+        <div className="loginWrapper">
+          <input
+            className="inputId"
+            type="text"
+            placeholder="이메일을 입력해주세요"
+            onChange={handleIdInput}
+          />
+          <span className="separator" />
+        </div>
+        <div className="topEmail">비밀번호</div>
+        <div className="loginWrapper">
+          <input
+            className="inputPw"
+            type="password"
+            placeholder="비밀번호을 입력해주세요"
+            onChange={handlePwInput}
+          />
+          <span className="separator" />
+        </div>
 
-          <button
-            className={isBtnActive ? 'loginButtonActive ' : 'loginButton'}
-          >
-            로그인
-          </button>
-        </form>
-        <div className="socialLogin">
-          <button className="kakaoLoginButton">
-            <i class="fa-regular fa-1x fa-comment" />
-            카카오 로그인
-          </button>
-          <button className="naverLoginButton">
-            <i class="fa-brands fa-1x fa-neos" />
-            네이버 로그인
-          </button>
-          <button className="googleLoginButton">
-            <i className="fa-brands fa-1x fa-google" />
-            구글 로그인
-          </button>
-        </div>
-        <div className="linkWrap">
-          <a className="findId" href="">
-            아이디 찾기
-          </a>
-          <a className="findPw" href="">
-            비밀번호 찾기
-          </a>
-          <button className="memberJoin">회원가입</button>
-        </div>
+        <button className={isBtnActive ? 'loginButtonActive ' : 'loginButton'}>
+          로그인
+        </button>
+      </form>
+      <div className="socialLogin">
+        <button className="kakaoLoginButton">
+          <i class="fa-regular fa-1x fa-comment" />
+          카카오 로그인
+        </button>
+        <button className="naverLoginButton">
+          <i class="fa-brands fa-1x fa-neos" />
+          네이버 로그인
+        </button>
+        <button className="googleLoginButton">
+          <i className="fa-brands fa-1x fa-google" />
+          구글 로그인
+        </button>
+      </div>
+      <div className="linkWrap">
+        <a className="findId" href="">
+          아이디 찾기
+        </a>
+        <a className="findPw" href="">
+          비밀번호 찾기
+        </a>
+        <button className="memberJoin">회원가입</button>
       </div>
     </div>
   );
