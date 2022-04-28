44b32d3d573fbbd496d1b494d5a92825a7cd1c6a
leehyoungjoon <92970761+leehyoungjoon@users.noreply.github.com>
2022-04-28 16:34:41 +0900

    정보 케러셀 창 구현 중

--- src/pages/Subscribe/Subscribe.js
+++ src/pages/Subscribe/Subscribe.js
@@ -5,14 +5,15 @@ const Subscribe = () => {
   // const clickhandler = ({ target }) => {
   //   target.className === 'leftIcon' ? setIndex(index - 1) : setIndex(index + 1);
   // };
-  const clickhandler = ({ target }) => {
+  const leftclickhandler = ({ target }) => {
     return setIndex(index - 1);
   };
-  const clickhandlerr = ({ target }) => {
+  const rightclickhandler = ({ target }) => {
     return setIndex(index + 1);
   };
 
-  console.log(index);
+  // console.log(index);
+
   return (
     <div className="mainWrap">
       <div className="topWrap">
@@ -116,7 +117,7 @@ const Subscribe = () => {
       <div className="dontWorry">걱정 마세요!</div>
       <div className="dontWorryWrapper">
         <div className="leftIcon">
-          <button onClick={clickhandler} />
+          <button onClick={leftclickhandler} />
         </div>
         <div className="dontWorrySlideBox">
           <div
@@ -128,15 +129,9 @@ const Subscribe = () => {
           >
             <div className="dontWorryBox">
               <img src="./images/landing-slide-icon2.png" />
-              <div>노련한 </div>
-              <div>매달 블라인드 테이스팅을 통해</div>
-              <div>누군가의 '인생술'이 될 수 있는 술들을 선별합니다.</div>
-            </div>
-            <div className="dontWorryBox">
-              <img src="./images/landing-slide-icon2.png" />
-              <div>노련한 소믈리에</div>
-              <div>매달 블라인드 테이스팅을 통해</div>
-              <div>누군가의 '인생술'이 될 수 있는 술들을 선별합니다.</div>
+              <div>결제되기 3일 전</div>
+              <div>이번 달 담와박스 구성의 힌트와</div>
+              <div>결제 알림 문자를 보내드려요.</div>
             </div>
             <div className="dontWorryBox">
               <img src="./images/landing-slide-icon2.png" />
@@ -144,9 +139,15 @@ const Subscribe = () => {
               <div>매달 블라인드 테이스팅을 통해</div>
               <div>누군가의 '인생술'이 될 수 있는 술들을 선별합니다.</div>
             </div>
+            <div className="dontWorryBox">
+              <img src="./images/landing-slide-icon2.png" />
+              <div>이번 달 구독이 힘들 것 같다면</div>
+              <div>언제든 '쉬어가기' 할 수 있어요.</div>
+              <div>당연히, 금액은 청구되지 않아요.</div>
+            </div>
           </div>
           <div className="rightIcon">
-            <button onClick={clickhandlerr} />
+            <button onClick={rightclickhandler} />
           </div>
           <ul className="buttonWrapper">
             <li className="slideButton" />
