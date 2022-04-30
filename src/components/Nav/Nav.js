b98a48e68ff46a2382be7e5a1f7f3e3726ecbf02
Marco <jonghyeok.lim14@gmail.com>
2022-04-30 23:54:51 +0900

    [32기 임종혁 ]nav 작업완료 및 리뷰요청

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.scss';
 
 const Nav = () => {
-  return <div>nav</div>;
+  return (
+    <header className="navHeader">
+      <nav className="navBar">
+        <Link to="/">
+          <div className="navLogo">
+            <img alt="logo" src="/images/logo.jpg" className="logoImg" />
+          </div>
+        </Link>
+        <div className="navLogin">
+          <Link to="subscribe">
+            <div className="subscribeBtn">구독서비스</div>
+          </Link>
+          <Link to="/">
+            <div className="storeBtn">담아마켓(스토어)</div>
+          </Link>
+          <Link to="/login">
+            <div className="loginBtn">로그인</div>
+          </Link>
+          <div className="logoutBtn">로그아웃</div>
+          <Link to="/cart">
+            <i className="fa-solid fa-1x fa-cart-shopping" />
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
