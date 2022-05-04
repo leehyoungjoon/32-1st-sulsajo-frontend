519aea294ffcd9f7e44ab752df3891267cf33ba1
Marco <jonghyeok.lim14@gmail.com>
2022-05-04 17:07:09 +0900

    [32기 임종혁 ]모달 스크롤방지 완료

--- src/pages/Main/components/SearchModal/SearchModal.js
+++ src/pages/Main/components/SearchModal/SearchModal.js
@@ -7,6 +7,19 @@ const SearchModal = ({ closeModal }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, []);
+
+  useEffect(() => {
     fetch('http://10.58.3.97:8000/products/list')
       .then(res => res.json())
       .then(data => {
